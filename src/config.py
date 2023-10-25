2c92b6ee086b77cb9d9c94032c6282ae435c3e87
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-10-25 19:36:25 +0300

    Change pinging interval to 10 sec

--- src/config.py
+++ src/config.py
@@ -4,6 +4,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
 IP = "8.8.8.8"
-PING_INTERVAL_IN_SEC = 30
+PING_INTERVAL_IN_SEC = 10
 
 MONGO_URL = "mongodb://mongodb:27017"
