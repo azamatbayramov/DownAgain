df7a1c1ede975f2b275a20d5a3e372e8c1730985
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-10-25 14:46:05 +0300

    Edit main.py

--- src/main.py
+++ src/main.py
@@ -1,23 +1,31 @@
-from pythonping import ping
 from time import sleep
 from aiogram import Bot
 import asyncio
 from src.config import BOT_TOKEN, CHANNEL_ID
 
+from src.database.db import init_db
+from src.database.models.ping_result import PingResultDB
+
+from src.ping import ping_and_save
 
 IP = "8.8.8.8"
 INTERVAL = 30
 
 bot = Bot(BOT_TOKEN)
 
-async def main():
-    while True:
-        response_list = ping(IP, verbose=True)
 
-        if not response_list.success():
-            await bot.send_message(CHANNEL_ID, "Oh, no, Internet Fell Again")
+async def main():
+    init_db()
+
+    while True:
+        await ping_and_save()
+
+        ping_result = await PingResultDB.get_last_ping_result()
+
+        await bot.send_message(CHANNEL_ID, str(ping_result))
 
         sleep(INTERVAL)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
