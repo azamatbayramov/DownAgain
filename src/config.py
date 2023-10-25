932b2a8ef002f6771a850770c0f2a357ce12e7b0
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-10-25 21:42:53 +0300

    Add timezone

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 import os
+from tzlocal import get_localzone_name
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
@@ -7,3 +8,5 @@ IP = "8.8.8.8"
 PING_INTERVAL_IN_SEC = 10
 
 MONGO_URL = "mongodb://mongodb:27017"
+
+TIMEZONE = get_localzone_name()
