78e0ba88dc7a01166af402851438c574bf9f0005
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-10-25 19:10:34 +0300

    Create function for formatting messages

---
+++ src/messages.py
@@ -0,0 +1,24 @@
+from src.database.models.internet_down import InternetDown
+
+from datetime import timedelta
+
+
+def format_duration(duration: timedelta) -> str:
+    seconds = duration.total_seconds()
+
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+
+    return f"{hours}h {minutes}m {seconds}s"
+
+
+async def format_internet_down_message(internet_down: InternetDown) -> str:
+    start_datetime = (await internet_down.get_start_datetime()).strftime("%d.%m.%Y %H:%M:%S")
+    end_datetime = (await internet_down.get_end_datetime()).strftime("%d.%m.%Y %H:%M:%S")
+
+    duration = format_duration(await internet_down.get_duration())
+
+    return f"#{internet_down.id} Internet was down again!\n"\
+           f"From {start_datetime} to {end_datetime}!.\n"\
+           f"Duration: {duration}."
