b77db6a457f619dcacbd1c2a2d217d34251041e6
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-10-25 15:29:14 +0300

    Remove verbose pinging, fix error

--- src/ping.py
+++ src/ping.py
@@ -8,8 +8,8 @@ from src.config import IP
 
 async def ping_and_save():
     ping_time = datetime.datetime.now()
-    response_list = ping(IP, verbose=True)
+    response_list = ping(IP)
 
-    ping_result = PingResult().fill(IP, ping_time, response_list)
+    ping_result = await PingResult.construct(IP, ping_time, response_list)
 
     await ping_result.insert()
