eec845dd5b3bf534f55eab5ceb6d266499af6083
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-10-25 19:17:55 +0300

    Add logic for saving and sending internet downs

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 from time import sleep
 from aiogram import Bot
 import asyncio
-from src.config import BOT_TOKEN, CHANNEL_ID
+from src.config import BOT_TOKEN, CHANNEL_ID, IP, PING_INTERVAL_IN_SEC
 
 from src.database.db import init_db
-from src.database.models.ping_result import PingResultDB
+from src.database.models.ping_result import PingResult, PingResultDB
+from src.database.models.internet_down import InternetDown, InternetDownDB
 
-from src.ping import ping_and_save
+from datetime import datetime
+from pythonping import ping
 
-IP = "8.8.8.8"
-INTERVAL = 30
+from messages import format_internet_down_message
 
 bot = Bot(BOT_TOKEN)
 
@@ -18,13 +19,34 @@ async def main():
     await init_db()
 
     while True:
-        await ping_and_save()
+        # Ping
+        ping_time = datetime.datetime.now()
+        response_list = ping(IP)
 
-        ping_result = await PingResultDB.get_last_ping_result()
+        # Save ping result into DB
+        ping_result = await PingResult.construct(IP, ping_time, response_list)
 
-        await bot.send_message(CHANNEL_ID, str(ping_result))
+        await ping_result.insert()
 
-        sleep(INTERVAL)
+        # Get info about current ping result and current internet down
+        current_internet_down = await InternetDownDB.get_current_internet_down()
+        current_ping_result = await PingResultDB.get_last_ping_result()
+
+        # If internet was not down and current ping was not success
+        if current_internet_down is None and not current_ping_result.success:
+            # Create new internet down and insert it into DB
+            internet_down = await InternetDown.construct(current_ping_result.id)
+
+            await internet_down.insert()
+        # If internet was down and current ping was success
+        elif current_internet_down is not None and current_ping_result.success:
+            # End current internet down
+            await current_internet_down.end(current_ping_result.id)
+
+            # Send message about internet down
+            bot.send_message(CHANNEL_ID, format_internet_down_message(current_internet_down))
+
+        sleep(PING_INTERVAL_IN_SEC)
 
 
 if __name__ == "__main__":
