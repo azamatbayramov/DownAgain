f59ecc8879c1a5a8c59e8f679109ffd809626ed1
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-10-25 14:46:15 +0300

    Delete unnecessary code in db.py

--- src/database/db.py
+++ src/database/db.py
@@ -1,10 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 
-from pythonping.executor import ResponseList
-
-from datetime import datetime
-
 from src.database.models.ping_result import PingResult
 
 from src.config import MONGO_URL
@@ -16,20 +12,3 @@ async def init_db():
         database=client.FellAgain,
         document_models=[PingResult]
     )
-
-
-async def save_ping_info_into_db(ip: str, ping_time: datetime, response_list: ResponseList):
-    ping_result = PingResult(
-        ip=ip,
-        datetime=ping_time,
-
-        success=response_list.success(),
-
-        rtt_min_ms=response_list.rtt_min_ms,
-        rtt_avg_ms=response_list.rtt_avg_ms,
-        rtt_max_ms=response_list.rtt_max_ms,
-
-        packet_loss=response_list.packet_loss
-    )
-
-    await ping_result.insert()
