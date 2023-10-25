fb0d8b095b8b9661cac885ced4504e022e359f56
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-10-25 23:13:55 +0300

    Add Mongo Express

--- src/config.py
+++ src/config.py
@@ -6,6 +6,6 @@ CHANNEL_ID = os.getenv("CHANNEL_ID")
 IP = "8.8.8.8"
 PING_INTERVAL_IN_SEC = 10
 
-MONGO_URL = "mongodb://mongodb:27017"
+MONGO_URL = "mongodb://mongo:27017"
 
 TIMEZONE = os.getenv("TIMEZONE")
