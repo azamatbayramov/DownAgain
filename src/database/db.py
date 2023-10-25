c232b74d37e4bcd485c3f91b34c53759e00c079c
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-10-25 19:31:42 +0300

    Add InternetDown for initialization

--- src/database/db.py
+++ src/database/db.py
@@ -2,6 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 
 from src.database.models.ping_result import PingResult
+from src.database.models.internet_down import InternetDown
 
 from src.config import MONGO_URL
 
@@ -10,5 +11,5 @@ async def init_db():
     client = AsyncIOMotorClient(MONGO_URL)
     await init_beanie(
         database=client.DownAgain,
-        document_models=[PingResult]
+        document_models=[PingResult, InternetDown]
     )
