cc1737a3ff309b4e7def69620ca4bae64b6c029a
Azamat Bayramov <bayramov.azamat04@gmail.com>
2023-10-25 23:19:41 +0300

    Add third container to reset_all sh script

--- reset_all.sh
+++ reset_all.sh
@@ -4,10 +4,12 @@ if [[ $confirm =~ ^[Yy]$ ]]; then
     echo "Stopping all containers..."
     sudo docker container stop down_again_bot
     sudo docker container stop down_again_mongodb
+    sudo docker container stop down_again_mongo_express
 
     echo "Removing all containers..."
     sudo docker container rm down_again_bot
     sudo docker container rm down_again_mongodb
+    sudo docker container rm down_again_mongo_express
 
     echo "Removing database volume..."
     sudo docker volume rm downagain_db_data
